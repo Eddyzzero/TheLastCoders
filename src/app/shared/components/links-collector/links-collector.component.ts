9a796d6305539a1ea499c261b034e86390a84301
Jhon Florez <jhon.florezr@gmail.com>
2025-03-26 15:34:31 +0100

    added new feature file linksCollector

---
+++ src/app/shared/components/links-collector/links-collector.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-links-collector',
+  imports: [],
+  templateUrl: './links-collector.component.html',
+  styleUrl: './links-collector.component.css'
+})
+export class LinksCollectorComponent {
+
+}
