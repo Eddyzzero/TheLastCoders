f819383e707a81524e427d7b612d7e528ffe5a50
Jhon Florez <jhon.florezr@gmail.com>
2025-04-04 13:46:12 +0200

    added into develop before main

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-// Import the functions you need from the SDKs you need for firebase
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
